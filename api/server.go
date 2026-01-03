d2926e166890422cb8e6484d1f3f24c3ce8b7e3f
AnshKumar200 <ansh0kumar07@gmail.com>
2026-01-03 13:07:56 +0530

    port fix

--- api/server.go
+++ api/server.go
@@ -1,12 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func startServer() {
 	http.HandleFunc("/ws", handleWebSocket)
 	http.HandleFunc("/health", handleHealthCheck)
 
-	http.ListenAndServe(":7878", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "7878"
+	}
+
+	addr := "0.0.0.0:" + port
+	log.Println("Listening on ", addr)
+
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
